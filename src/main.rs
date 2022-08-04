ef72c73e3413163f53870bb9acc1dd483fa0c9b0
nathiss <kamil.jakub.rusin@gmail.com>
2022-08-04 22:07:47 +0200

    Add logger to the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use log::info;
+
+static BANNER: &str = include_str!("asserts/banner.txt");
+
+fn initialize_logger() -> Result<(), anyhow::Error> {
+    let log_config_result = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S%.3f]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("log/default.log")?)
+        .apply();
+
+    match log_config_result {
+        Ok(_) => Ok(()),
+        Err(e) => Err(anyhow::Error::new(e)),
+    }
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    initialize_logger()?;
+
+    info!("{}", BANNER);
+
+    Ok(())
 }
